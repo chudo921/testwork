dcfb22508742a37bcd10205e8ccfb55cad7c4bdf
Oleg Polyakov <polyakov.gelo@yandex.ru>
2022-10-11 21:01:44 +0300

    java

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Корзина приложения");
+
+        int basket = 0;
+        int maxMoney = 300000;
+        int mixMoney = 1;
+        System.out.println("Контакты для связи:  +7 900(961)13-47");
+        System.out.println("E-mail:  polyakov.gelo@yandex.ru");
+    }
+}
